553f78fd08974b66b766657a9aaed23a57ef6464
landy <14ndy15@gmail.com>
2019-10-23 22:01:36 -0400

    adding two libraries for later improvement

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,11 +1,15 @@
+import sal from 'sal.js/dist/sal';
 import 'lazysizes/plugins/unveilhooks/ls.unveilhooks.min';
 import SmoothScroll from 'smooth-scroll';
 import MobileMenu from './modules/_menuMobile';
 
+new MobileMenu();
 new SmoothScroll('a[href*="#"]', {
     speed: 500,
     speedAsDuration: true
 });
+sal();
 
-new MobileMenu();
+
+
 
